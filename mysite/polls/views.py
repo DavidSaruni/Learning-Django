5844063400eb1945119462fabf20e7717f984260
DavidSaruni <sarunidavid11126@gmail.com>
2022-11-11 10:56:26 +0300

    Models and Templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
-def index(request):
-    return HttpResponse("Hello, Captain. You are at the polls index")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, "main/base.html", {})
+
+def index2(response):
+    return HttpResponse("This is index 2!")
+
+def home(response):
+    return render(response, "main/home.html", {})
