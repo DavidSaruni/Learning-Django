1572c15257602087e0509f79b7d4f7622a285392
DavidSaruni <sarunidavid11126@gmail.com>
2022-11-14 01:26:52 +0300

    Custom Forms

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 # from polls import forms
 from .forms import CreateNewList
@@ -7,6 +7,29 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    # check if we have posted anything
+    if response.method == "POST":
+        print(response.POST)
+        # check if we have saved an item
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
+
     return render(response, "main/list.html", {"ls":ls})
 
 def index2(response):
@@ -16,5 +39,14 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def create(request):
-    form = CreateNewList()
+    if request.method == "POST":
+        form = CreateNewList(request.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" %t.id)
+    else:    
+        form = CreateNewList()
     return render(request, "main/create.html", {"form":form})
